480812e4aaf2a1f416f6bd3f4eac618a223f90f5
ayeshatabassum1406 <64879625+ayeshatabassum1406@users.noreply.github.com>
2023-10-23 00:11:01 +0530

    java2

---
+++ backslash.java
@@ -0,0 +1,15 @@
+import javax.sql.rowset.spi.SyncResolver;
+
+public class backslash {
+    public static void main(String[] args) {
+        System.out.println("\' this is single quote");
+        System.out.println("\" this is double quote");
+        System.out.println("\\ this is backslash ");
+        System.out.println("this is \n new line ");
+        System.out.println("this is \r carriage returns");
+        System.out.println("this is \t tab");
+        System.out.println("this is \b backspace");
+        System.out.println("this is \f formfeed");
+    }
+    
+}
