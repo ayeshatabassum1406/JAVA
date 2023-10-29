8b82dd51033168ea4b077db914623d96f88f1b96
ayeshatabassum1406 <64879625+ayeshatabassum1406@users.noreply.github.com>
2023-10-29 19:28:40 +0530

    day3

---
+++ functions.java
@@ -0,0 +1,13 @@
+public class functions {
+    //class deckaration
+    static void func(){//static:belongs to main class but nott objectof it, void:returns nothing
+System.out.println("this is function");
+    }
+    public static void main(String[] args) {
+       System.out.println("this is main, calling function"); 
+       // calling function
+       func();
+       System.out.println("call is done");
+
+    }
+}
