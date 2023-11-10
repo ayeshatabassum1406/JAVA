7009bcbbdea0b6c5e2f0284789142b9ce864fe5e
AYESHA TABASSUM <64879625+ayeshatabassum1406@users.noreply.github.com>
2023-11-10 09:17:49 -0500

    day5

---
+++ pyramidpattern.java
@@ -0,0 +1,19 @@
+public class pyramidpattern {
+    public static void main(String[] args) {
+        // reverse pattern
+        int rows=8;
+        //below nested loops will create spaces on LHS of pyramid
+        for(int i=0;i<rows-1;i++)
+        {
+            for(int j=0;j<=i;j++){
+               System.out.print(" ");
+            }
+            //below lines will disply reverse pyramid
+            for(int k=0;k<=rows-2-i;k++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
